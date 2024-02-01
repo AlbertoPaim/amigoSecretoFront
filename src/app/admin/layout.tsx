3b5a0c06a9e9e41678de140c243eca70907f4021
Alberto Paim <albertoopaim@gmail.com>
2024-02-01 20:14:05 -0300

    fix layout2

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,27 +1,19 @@
-import { Metadata } from "next";
 import { ReactNode } from "react";
 
-export const metadata: Metadata = {
-	title: 'Amigo Secreto - Admin'
-}
+type Props = { children: ReactNode };
 
-type Props = { children: ReactNode }
-
-export const layout = ({ children }: Props) => {
+const Layout: React.FC<Props> = ({ children }) => {
 	return (
 		<div>
 			<header className="bg-gray-800 text-center py-5">
 				<h3 className="text-3xl">Amigo Secreto</h3>
 				<h4 className="text-base">Painel de Controle</h4>
-
 			</header>
 			<main className="mx-auto w-full max-w-3xl p-3">
 				{children}
 			</main>
-
-
 		</div>
-	)
+	);
 };
 
-export default layout;
+export default Layout;
